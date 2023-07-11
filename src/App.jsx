74568725347d8d66a69892d47f9890c9b08f80ca
Khizar Ali <khizibutt123@gmail.com>
2023-07-11 10:54:42 +0500

    Adding skills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./Components/about/About";
 import Header from "./Components/header/header";
 import Home from "./Components/home/Home";
+import Skills from "./Components/skills/Skills";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main className="main">
         <Home />
         <About />
+        <Skills />
       </main>
     </>
   );
---
+++ src/Components/skills/Skills.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Skills.css";
+import FrontEnd from "./FrontEnd";
+import BackEnd from "./BackEnd";
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+      <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">My technical level</span>
+      <div className="skills__container container grid">
+        <FrontEnd />
+        <BackEnd />
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
